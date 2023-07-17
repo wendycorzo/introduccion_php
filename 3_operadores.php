cd67b37c95ed644972040c60029053f843612eef
jennyromero <jennyromero@colegioguanenta.com>
2023-07-17 18:38:35 -0500

    commit

---
+++ 3_operadores.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>operadores</title>
+</head>
+<body>
+<h1>Operadores</h1>
+    <!--script php-->
+    <?php 
+    /*1.Los operadores son símbolos que se utilizan en la elaboración 
+    de una ecuación de búsqueda y que permiten combinar diferentes términos entre sí y establecer relaciones lógicas entre los términos*/ 
+
+    /*2.operadores es que son elementos que relacionan de forma diferente, los valores de una o más variables y/o constantes*/ 
+
+    /*3.Los operadores son elementos que relacionan de forma diferente, los valores con los que
+ trabajamos en los lenguajes de programación. En los lenguajes de programación usamos los operadores para manipular los valores y transformarlos, con el objetivo de realizar 
+ los objetivos de los programas*/
+</body>
+</html>
