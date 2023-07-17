f9c5a252ac8933fa1678984207a1a78bd44c05e9
jennyromero <jennyromero@colegioguanenta.com>
2023-07-17 17:30:24 -0500

    comentarios en php

---
+++ 1_comentarios.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comentarios en PHP</title>
+</head>
+<body>
+    <h1>Comentarios en PHP</h1>
+    <! Formas de incluir codigo php --> 
+
+    <?php
+       //comentario de una linea
+       /*Esto es un comentario de
+       varias lineas */
+    ?>
+
+    <!--
+    <%
+       codigo php
+    %>
+
+    <script languaje="<php">
+
+</script>-->
+
+</body>
+</html>
