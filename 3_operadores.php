61e8b713f6507932f48481f74d79f7962011b2bd
jennyromero <jennyromero@colegioguanenta.com>
2023-07-18 06:40:04 -0500

    commit

--- 3_operadores.php
+++ 3_operadores.php
@@ -12,33 +12,23 @@
     <?php 
     operadores aritmeticos
     /*1.Sirven para hacer cálculos matemáticos con valores numéricos*/ 
-    $sumar = 1 + 1
-$restar = 20 - 5
-$multiplicar = 15 * 2
-$dividir = 30 / 30
-$potencia = 2 ^ 2
+    <?php
+
+echo (5 % 3)."\n";           // muestra 2
+echo (5 % -3)."\n";          // muestra 2
+echo (-5 % 3)."\n";          // muestra -2
+echo (-5 % -3)."\n";         // muestra -2
+
+?>
 
     Operadores de concatenación:
     /*2.Sirven para combinar varias cadenas dentro de una sola*/  
-    $y_1_1 = verdadero y verdadero  # -> verdadero
-$y_1_0 = verdadero y falso  # -> falso
-$y_0_1 = falso y verdadero  # -> falso
-$y_0_0 = falso y falso  # -> falso
-
-$o_1_1 = verdadero o verdadero # -> verdadero
-$o_1_0 = verdadero o falso  # -> verdadero
-$o_0_1 = falso o verdadero  # -> verdadero
-$o_0_0 = falso o falso  # -> falso
-
-$no_1 = no verdadero  # -> falso
-$no_0 = no falso  # -> verdadero
-
-    Operadores de comparación:
-    /*3.Sirven para comparar dos expresiones y devolver un valor Boolean que representa
-     el resultado de la comparación. Ejemplo: Aquí, 10 se compara con 3 usando el operador
-      «más grande que». El resultado de esta operación, es decir, el valor lógico verdadero,
-       se guarda en la variable $respuesta*/
-     $respuesta = 10 > 3
-
+    <?php
+    $a = "Hello ";
+    $b = $a . "World!"; // ahora $b contiene "Hello World!"
+    
+    $a = "Hello ";
+    $a .= "World!";     // ahora $a contiene "Hello World!"
+    ?>
 </body>
 </html>
